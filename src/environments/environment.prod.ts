cb5473b60f868236bbd7f0710f4d741a6e758aa1
hectorbmx <hectorbmx@gmail.com>
2026-02-02 22:49:02 -0600

    feat(ios): healtKit stage 1 read-only + dayly progrese

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  //  apiUrl: 'http://127.0.0.1:8000/api/v1',  //local en navegador
-  // apiUrl: 'http://10.0.2.2:8000/api/v1'
-  // apiUrl: 'http://10.0.2.2:8000/api/v1' //local en emulador
-  
-  apiUrl:'https://bmxmexico.com/coach/public/api/v1' //prod
+  //  apiUrl: 'http://127.0.0.1:8000/api/v1', 
+  apiUrl: 'http://10.0.2.2:8000/api/v1'
 };
